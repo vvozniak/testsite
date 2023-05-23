1ec5a8e5a8e0f29503d23b7f32c27fb405135289
vvozniak <102913255+vvozniak@users.noreply.github.com>
2023-05-23 21:05:51 +0200

    Add files via upload

---
+++ assets/js/main2.js
@@ -0,0 +1,12 @@
+var typed5 = new Typed('#typing', {
+    strings: ['Zdjęcia', 'Filmy', 'Montaż', 'Grafika', 'Maybe Movie?'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    startDelay: 1000,
+    cursorChar: '_',
+    shuffle: true,
+    smartBackspace: false,
+    loop: true
+  });
+
